4555027fc9293f5d1fdf7e47285fb9081b32e9ad
Your Name <charles.luas.young@gmail.com>
2021-11-12 15:56:31 -0700

    Create book component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import './App.css';
 import TitleBar from "./TitleBar/TitleBar";
 import BookViewer from "./BookViewer/BookViewer";
+import BookCreator from "./BookCreator/BookCreator";
 
 class App extends Component {
     constructor(props){
@@ -38,12 +39,17 @@ class App extends Component {
         });
     }
 
+    createBook = (newBook) => {
+        console.log('From the createBook on App component', newBook);
+        this.books.push(newBook);
+    }
+
     render(){
         return(
             <div className="container-fluid">
                 <TitleBar/>
                 <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook}/>
-                
+                <BookCreator createNewBook={this.createBook}/>
             </div>
         )
     }
---
+++ src/components/BookCreator/BookCreator.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+
+class BookCreator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            author:''
+        }
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.createNewBook(this.state)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Book Name</label>
+                <input name='title' onChange={this.handleChange} value={this.state.title}/>
+                <label>Author</label>
+                <input name='author' onChange={this.handleChange} value={this.state.author}/>
+                <button typ='submit'>Create Book</button>
+            </form>
+        );
+    }
+}
+export default BookCreator;
